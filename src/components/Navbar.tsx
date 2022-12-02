a551b364bc152f024a2187e9c553f1dd6db909e0
Joao Pedro Portes Fernandes <thejportes@gmail.com>
2022-12-02 01:55:22 -0300

    shit fuck

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 
+
 const Navbar = () => {
   return (
-    <div className="flex gap-4 font-zilla font-bold">
-      <h1 className="text-white  text-3xl">HOME</h1>
-      <h1 className="text-white  text-3xl">MAP</h1>
-      <h1 className="text-white  text-3xl">SHOWROOM</h1>
+    <div className="flex justify-between w-full items-center">
+      <div className="flex gap-10 font-zilla font-bold items-center w-1/3">
+        <h1 className="text-white  text-3xl">HOME</h1>
+        <h1 className="text-white  text-3xl">MAP</h1>
+        <h1 className="text-white  text-3xl">SHOWROOM</h1>
+      </div>
+      
+      <div className="bg-background py-4 px-6 rounded-xl
+      bg-opacity-20 backdrop-blur-xs flex gap-10 items-center">
+        
+      <div className="border-b-[6px] border-solid border-secondary rounded-2xl pb-[2px]">
+        <h2 className=" font-zilla font-bold title-font mt-4 leading-snug tracking-wider  rounded-2xl text-black text-5xl border-b-[6px] border-solid border-primary mb-1">
+          SHARPOETRY
+        </h2>
+      </div>
+      </div>
     </div>
   );
 };
--- src/components/SocialMediaIcons.tsx
+++ src/components/SocialMediaIcons.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-import { FaTwitter, FaLinkedin } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 import { VscGithub } from "react-icons/vsc"
 
 const SocialMediaIcons = () => {
   return (
-    <div className="flex flex-col md:flex-row sm:flex-row xl:flex-row gap-3  justify-center items-center">
-      <a href="https://github.com/PortsZ"><VscGithub/></a>
-      <a href="https://www.linkedin.com/in/joao-pedro-portes-301364184/"><FaLinkedin/></a>  
-      <a href="https://twitter.com/PortsZ_"><FaTwitter/></a>  
+    <div className="flex flex-col md:flex-row sm:flex-row xl:flex-row gap-3  justify-start items-center ">
+      <a target="_blank" href="https://www.instagram.com/sharpoetry/">
+        <FaInstagram size='50px'/></a>
       
     </div>
   );
